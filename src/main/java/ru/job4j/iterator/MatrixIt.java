de282c96a1e0ceaad137c8d84172941a73c68c0a
Sarabik <dalilija@inbox.lv>
2022-08-09 22:13:22 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539]

---
+++ src/main/java/ru/job4j/iterator/MatrixIt.java
@@ -0,0 +1,38 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MatrixIt implements Iterator<Integer> {
+
+    private final int[][] data;
+    private int row = 0;
+    private int column = 0;
+
+    public MatrixIt(int[][] data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while (row < data.length && data[row].length == 0) {
+            row++;
+        }
+        return row < data.length;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int result = 0;
+        if (column == data[row].length - 1) {
+            result = data[row++][column];
+            column = 0;
+        } else {
+            result = data[row][column++];
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/iterator/MatrixItTest.java
@@ -0,0 +1,114 @@
+package ru.job4j.iterator;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.NoSuchElementException;
+
+class MatrixItTest  {
+
+    @Test
+    void when4El() {
+        int[][] in = {
+                {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next()).isEqualTo(1);
+    }
+
+    @Test
+    void whenFirstEmptyThenNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next()).isEqualTo(1);
+    }
+
+    @Test
+    void whenFirstEmptyThenHashNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenRowHasDiffSize() {
+        int[][] in = {
+                {1}, {2, 3}, {}, {}, {4}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.next()).isEqualTo(3);
+        assertThat(it.next()).isEqualTo(4);
+        assertThat(it.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenFewEmpty() {
+        int[][] in = {
+                {1}, {}, {}, {}, {2}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenEmpty() {
+        int[][] in = {
+                {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenEmptyThenNext() {
+        int[][] in = {
+                {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThatThrownBy(it::next)
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void whenMultiHashNext() {
+        int[][] in = {
+                {}, {1}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext()).isTrue();
+        assertThat(it.hasNext()).isTrue();
+    }
+
+    @Test
+    void whenNoElements() {
+        int[][] in = {
+                {}, {}, {}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.hasNext()).isFalse();
+    }
+
+    @Test
+    void whenNoEmptyElements() {
+        int[][] in = {
+                {1}, {2, 3}, {4}, {5, 6, 7}, {8}
+        };
+        MatrixIt it = new MatrixIt(in);
+        assertThat(it.next()).isEqualTo(1);
+        assertThat(it.next()).isEqualTo(2);
+        assertThat(it.next()).isEqualTo(3);
+        assertThat(it.next()).isEqualTo(4);
+        assertThat(it.next()).isEqualTo(5);
+        assertThat(it.next()).isEqualTo(6);
+        assertThat(it.next()).isEqualTo(7);
+        assertThat(it.next()).isEqualTo(8);
+        assertThat(it.hasNext()).isFalse();
+    }
+}
