e378fa101d3278c7472cfc2f52cafd8767b24db1
Sarabik <dalilija@inbox.lv>
2022-08-13 18:24:21 +0300

    2. Создать контейнер на базе связанного списка [#159]

---
+++ src/main/java/ru/job4j/collection/SimpleLinkedList.java
@@ -0,0 +1,79 @@
+package ru.job4j.collection;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+public class SimpleLinkedList<E> implements LinkedList<E> {
+
+    private int size;
+    private Node<E> first;
+    private Node<E> last;
+    private int modCount;
+
+    private static class Node<E> {
+        E item;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    @Override
+    public void add(E value) {
+        final Node<E> l = last;
+        final Node<E> newNode = new Node(l, value, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
+        modCount++;
+    }
+
+    @Override
+    public E get(int index) {
+        Objects.checkIndex(index, size);
+        Node<E> n = first;
+        for (int i = 0; i < index; i++) {
+            n = n.next;
+        }
+        return n.item;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int expectedModCount = modCount;
+            private Node<E> itElement = first;
+            private int counter = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return size != 0 && counter < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                E result = itElement.item;
+                itElement = itElement.next;
+                counter++;
+                return result;
+            }
+
+        };
+    }
+}
