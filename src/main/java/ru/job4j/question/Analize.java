00bd337b6ecc4a381953fbfcd41c3c5cc1cbec25
Sarabik <dalilija@inbox.lv>
2022-08-24 21:13:01 +0300

    2. Статистика по коллекции. [#45889 #335788]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,26 +1,23 @@
 package ru.job4j.question;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
+        Set<User> combineSet = new TreeSet<>(previous);
+        combineSet.addAll(current);
         int changed = 0;
-        int remaining = 0;
-        for (User user1 : previous) {
-            for (User user2 : current) {
-                if ((!user1.equals(user2)) && user1.getId() == user2.getId()) {
-                    changed++;
-                    break;
-                } else if (user1.equals(user2)) {
-                    remaining++;
-                    break;
-                }
+        User temp = new User(0, null);
+        for (User user : combineSet) {
+            if (temp.getId() == user.getId()) {
+                changed++;
             }
+            temp = user;
         }
-        return new Info(current.size() - remaining - changed,
+        return new Info(combineSet.size() - previous.size() - changed,
                 changed,
-                previous.size() - changed - remaining);
+                combineSet.size() - current.size() - changed);
     }
-
 }
--- src/main/java/ru/job4j/question/User.java
+++ src/main/java/ru/job4j/question/User.java
@@ -2,7 +2,7 @@ package ru.job4j.question;
 
 import java.util.Objects;
 
-public class User {
+public class User implements Comparable<User> {
 
     private int id;
     private String name;
@@ -31,5 +31,14 @@ public class User {
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        int compare = Integer.compare(id, o.id);
+        if (compare == 0) {
+            compare = name.compareTo(o.name);
+        }
+        return compare;
     }
 }
