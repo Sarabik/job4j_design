985fa7710787d9c558946086d5e697b79805cfbf
Sarabik <dalilija@inbox.lv>
2022-08-31 00:23:40 +0300

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,38 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<Path>> map = new HashMap<>();
+
+    public void getDublicates() {
+        for (Map.Entry<FileProperty, List<Path>> entry : map.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                System.out.printf("File name - \"%s\", size - %d byte%n",
+                        entry.getKey().getName(), entry.getKey().getSize());
+                System.out.println("Paths:");
+                for (Path path : entry.getValue()) {
+                    System.out.println(path.toAbsolutePath());
+                }
+            }
+        }
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.getFileName().toString());
+        map.computeIfPresent(fileProperty, (key, value) -> {
+            value.add(file);
+            return value;
+        });
+        map.putIfAbsent(fileProperty, new ArrayList<Path>() {{
+            add(file);
+        }});
+        return super.visitFile(file, attrs);
+    }
+}
