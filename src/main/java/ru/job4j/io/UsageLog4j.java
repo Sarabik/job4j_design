4aac95fa20fd112f1a0a9264e985c7ebbafc719a
Sarabik <dalilija@inbox.lv>
2022-09-08 22:44:35 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,16 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        double weight = 59.9;
+        float height = 1.71f;
+        int floor = 4;
+        short house = 42;
+        byte flat = 12;
+        char section = 'a';
+        long distance = 32000;
+        boolean isTrue = true;
+        LOG.debug("Anton: weight - {} kg, height - {} cm. He lives on {} floor.", weight, height, floor);
+        LOG.debug("Adress: Salnas str. {}/{} - {}. Distance from Moscow = {} km", house, section, flat, distance);
+        LOG.debug("Info = {}", isTrue);
     }
 }
