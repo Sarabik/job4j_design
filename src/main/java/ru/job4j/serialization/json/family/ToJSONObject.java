26fbd24752618f9873b98d2ab17941c53835b847
Sarabik <dalilija@inbox.lv>
2022-09-11 17:09:35 +0300

    5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/family/ToJSONObject.java
+++ src/main/java/ru/job4j/serialization/json/family/ToJSONObject.java
@@ -1,4 +1,17 @@
 package ru.job4j.serialization.json.family;
 
+import org.json.JSONObject;
+
 public class ToJSONObject {
+    public static void main(String[] args) {
+        Family family = new Family("Ivanovi",
+                new String[]{"Ivan", "Olga", "Lisa"},
+                new Address("Moscow", "Markova", 52),
+                true);
+
+        /*serialization*/
+        JSONObject jsonObj = new JSONObject(family);
+        String json = jsonObj.toString();
+        System.out.println(json);
+    }
 }
