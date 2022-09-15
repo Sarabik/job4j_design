5ffee7dd9d7a53a43b1890666af8c156b9f5f050
Sarabik <dalilija@inbox.lv>
2022-09-15 09:22:17 +0300

    2. Поиск файлов по критерию [#783 #343616]

--- src/main/java/ru/job4j/io/filesearcher/SearchType.java
+++ src/main/java/ru/job4j/io/filesearcher/SearchType.java
@@ -10,43 +10,24 @@ public class SearchType {
         if ("name".equals(searchType)) {
             condition = file -> fileName.equals(file.toFile().getName());
         } else if ("mask".equals(searchType)) {
-            condition = file -> maskCheck(file.toFile().getName(), fileName);
+            condition = file -> Pattern.matches(maskToRegex(fileName), file.toFile().getName());
         } else if ("regex".equals(searchType)) {
             condition = file -> Pattern.matches(fileName, file.toFile().getName());
         }
         return condition;
     }
 
-    private static boolean maskCheck(String file, String fileMask) {
-        char[] fileArray = file.toCharArray();
-        char[] maskArray = fileMask.toCharArray();
-        int fileCounter = 0;
-        int maskCounter = 0;
-        int starIndex = -1;
-        boolean result = false;
-        while (fileCounter < fileArray.length && maskCounter < maskArray.length) {
-            if (maskArray[maskCounter] == '*') {
-                starIndex = maskCounter;
-                maskCounter++;
-                if (maskCounter == maskArray.length) {
-                    fileCounter = fileArray.length;
-                }
-            } else if (maskArray[maskCounter] == '?' || maskArray[maskCounter] == fileArray[fileCounter]) {
-                result = true;
-                maskCounter++;
-                fileCounter++;
-            } else if (maskArray[maskCounter] != fileArray[fileCounter]) {
-                if (starIndex != -1 && result) {
-                    maskCounter = starIndex + 1;
-                } else if (starIndex == -1) {
-                    result = false;
-                    break;
-                } else {
-                    fileCounter++;
-                }
-                result = false;
-            }
+    private static String maskToRegex(String fileMask) {
+        StringBuilder builder = new StringBuilder();
+        for (char ch : fileMask.toCharArray()) {
+            String stringRegex = switch (ch) {
+                case '.' -> "\\.";
+                case '?' -> ".";
+                case '*' -> ".*";
+                default -> Character.toString(ch);
+            };
+            builder.append(stringRegex);
         }
-        return result && (fileCounter == fileArray.length && maskCounter == maskArray.length);
+        return builder.toString();
     }
 }
