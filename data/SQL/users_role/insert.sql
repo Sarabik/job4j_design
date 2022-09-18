ab69c41fecd30be2efe61ce88b2242d05335654a
Sarabik <dalilija@inbox.lv>
2022-09-18 17:25:29 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ data/SQL/users_role/insert.sql
@@ -0,0 +1,4 @@
+insert into role(name) values ('admin');
+insert into role(name) values ('user');
+insert into users(name, role_id) values ('Anton', 1);
+insert into users(name, role_id) values ('Olga', 2);
