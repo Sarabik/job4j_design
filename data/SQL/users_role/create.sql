ef41620a9eb1a4545e913621e8608b99753d084c
Sarabik <dalilija@inbox.lv>
2022-09-19 20:43:05 +0300

    1. Схема прав пользователи и роли. [#1093 #344715]

--- data/SQL/users_role/create.sql
+++ data/SQL/users_role/create.sql
@@ -13,7 +13,6 @@ create table rules (
 );
 create table role_rules (
 	id serial primary key,
-	name text,
 	role_id int references role(id),
 	rules_id int references rules(id)
 );
@@ -28,10 +27,16 @@ create table state (
 create table item (
 	id serial primary key,
 	name text,
+	users_id int references users(id),
 	category_id int references category(id),
 	state_id int references state(id)
 );
 create table comments (
+	id serial primary key,
+	name text,
+	item_id int references item(id)
+);
+create table attachs (
 	id serial primary key,
 	name text,
 	item_id int references item(id)
--- data/SQL/users_role/insert.sql
+++ data/SQL/users_role/insert.sql
@@ -2,3 +2,11 @@ insert into role(name) values ('admin');
 insert into role(name) values ('user');
 insert into users(name, role_id) values ('Anton', 1);
 insert into users(name, role_id) values ('Olga', 2);
+insert into rules(name) values ('Rule1');
+insert into category(name) values ('Category1');
+insert into state(name) values ('State1');
+insert into item(name, users_id, category_id, state_id) values ('NewItem', 1, 1, 1);
+insert into comments(name, item_id) values ('Comment1', 1);
+insert into attachs(name, item_id) values ('Attach1', 1);
+
+
