29161821cc21802469140cb8f17074fd67b9ebc2
Sarabik <dalilija@inbox.lv>
2022-09-19 20:45:05 +0300

    0. Select [#6861 #344741]

--- data/SQL/animals_filters/animals_init.sql
+++ data/SQL/animals_filters/animals_init.sql
@@ -9,3 +9,8 @@ insert into fauna(name, avg_age, discovery_date) values ('Lion', 4000, null);
 insert into fauna(name, avg_age, discovery_date) values ('Elephant', 13000, null);
 insert into fauna(name, avg_age, discovery_date) values ('Finifenmaa fish', 500, date '2022-09-03');
 insert into fauna(name, avg_age, discovery_date) values ('Puma', 3600, null);
+
+select * from fauna where name like '%fish%';
+select * from fauna where avg_age > 10000 and avg_age < 21000;
+select * from fauna where discovery_date is null;
+select * from fauna where discovery_date < date '1950-01-01';
