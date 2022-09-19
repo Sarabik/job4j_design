d107581b31654d812a54914f5163d697d91f95ed
Sarabik <dalilija@inbox.lv>
2022-09-19 19:12:50 +0300

    1. Inner join [#373617]

---
+++ data/SQL/inner join/inner_join.sql
@@ -0,0 +1,24 @@
+create table PostOffice (
+	id serial primary key,
+	POnumber int,
+	POindex int
+);
+create table Address (
+	id serial primary key,
+	address text,
+	PostOffice_id int references PostOffice(id)
+);
+
+insert into PostOffice(POnumber, POindex) values (57, 125057);
+insert into PostOffice(POnumber, POindex) values (430, 125430);
+insert into PostOffice(POnumber, POindex) values (490, 125490);
+insert into PostOffice(POnumber, POindex) values (47, 125047);
+insert into Address(address, PostOffice_id) values ('ул. Ленинградский проспект, дом 67, корп. 1', 9);
+insert into Address(address, PostOffice_id) values ('ул. Митинская, дом 28, корп. 3', 10);
+insert into Address(address, PostOffice_id) values ('ул. Пестеля, дом 11', 11);
+insert into Address(address, PostOffice_id) values ('ул. 1-я Тверская-Ямская, дом 29', 12);
+
+select a.address as Адрес, p.POnumber as "Номер почтового отделения", p.POindex as Индекс 
+from Address as a join PostOffice as p on a.PostOffice_id = p.id where a.address like '%ин%';
+select p.POindex, a.address from Address as a join PostOffice as p on a.PostOffice_id = p.id order by p.POindex asc;
+select p.POindex, a.address from PostOffice as p join Address as a on a.PostOffice_id = p.id where p.POindex = 125057;
